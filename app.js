fc6f5e8aea1b5587e7e307267c340886fd7c8b03
Núbia Segóvia <rsegovia.nubia@gmail.com>
2022-08-01 18:58:59 -0300

    [ADD] api data

---
+++ app.js
@@ -0,0 +1,20 @@
+const adviceNumber = document.querySelector(".advice-id");
+const adviceText = document.querySelector(".advice-text");
+const diceBtn = document.querySelector(".dice-btn");
+
+function generateAdvice() {
+    const apiURL = "https://api.adviceslip.com/advice";
+    fetch(apiURL)
+        .then(function (response) {
+            const data = response.json();
+            return data;
+        })
+        .then(function (data) {
+            adviceNumber.innerText = data.slip.id;
+            adviceText.innerHTML = `<q>${data.slip.advice}</q>`;
+        });
+}
+
+generateAdvice();
+
+diceBtn.addEventListener("click", generateAdvice);
